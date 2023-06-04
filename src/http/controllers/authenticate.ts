42c58532742a0d7272611640832a20b329c28f94
NatanRei <nreischmura@gmail.com>
2023-06-04 20:12:37 -0300

    feat: creation initial of authentication process

---
+++ src/http/controllers/authenticate.ts
@@ -0,0 +1,29 @@
+import { FastifyRequest, FastifyReply } from 'fastify'
+import { z } from 'zod'
+import { PrismaUsersRepository } from '../repositories/prisma/prisma-users-repository';
+import { AuthenticateUseCase } from '../use-cases/authenticate';
+import { InvalidCredentialsError } from '../use-cases/errors/invalid-credentials-error';
+
+export async function authenticate( request: FastifyRequest, reply: FastifyReply ) {
+    const authenticateBodySchema = z.object({
+        email: z.string().email(),
+        password: z.string().min(6)
+    })
+
+    const { email, password } = authenticateBodySchema.parse(request.body)
+    
+    try {
+        const usersRepository = new PrismaUsersRepository()
+        const registerUseCase = new AuthenticateUseCase(usersRepository)
+        await registerUseCase.execute({
+            email, password
+        })
+    } catch (err) {
+        if (err instanceof InvalidCredentialsError) {
+            return reply.status(400).send({ 'message': err.message })
+        }
+        throw err
+    }
+
+    return reply.status(200).send()
+}
