56ef32efc4ff84e0bd73d504bbc8172da5a5c9ab
NatanRei <nreischmura@gmail.com>
2023-06-04 21:22:51 -0300

    feat: base inicial do checkIn

--- src/http/repositories/in-memory/in-memory-users-repository.ts
+++ src/http/repositories/in-memory/in-memory-users-repository.ts
@@ -1,5 +1,6 @@
 import { Prisma, User } from "@prisma/client";
 import { UsersRepository } from "../users-repository";
+import { randomUUID } from "node:crypto";
 
 export class InMemoryUsersRepository implements UsersRepository {
     public items: User[] = []
@@ -24,7 +25,7 @@ export class InMemoryUsersRepository implements UsersRepository {
 
     async create(data: Prisma.UserCreateInput): Promise<User> {
         const user = {
-            id: 'user-1',
+            id: randomUUID(),
             name: data.name,
             email: data.email,
             password_hash: data.password_hash,
