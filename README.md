5fa25b1b009ec3062a29d260e066556722af63ec
NatanRei <nreischmura@gmail.com>
2023-06-12 07:07:29 -0300

    feat: não deixando o user fazer check in no mesmo dia

--- README.md
+++ README.md
@@ -19,7 +19,7 @@ GymPass style app.
 ## RNs (Regras de negócio)
 
 - [x] O usuário não deve poder se cadastrar com um e-mail duplicado;
-- [ ] O usuário não pode fazer 2 check-ins no mesmo dia;
+- [x] O usuário não pode fazer 2 check-ins no mesmo dia;
 - [ ] O usuário não pode fazer check-in se não estiver perto da academia;
 - [ ] O check-in só pode ser validado até 20 minutos após criado;
 - [ ] O check-in só pode ser validado por administradores
--- src/http/use-cases/check-in.ts
+++ src/http/use-cases/check-in.ts
@@ -14,6 +14,13 @@ export class CheckInUseCase {
     constructor (private checkInsRepository: CheckInsRepository) {}
 
     async execute({ userId, gymId }: CheckInUseCaseRequest): Promise<CheckInUseCaseResponse> {
+
+        const checkInOnSameDay = await this.checkInsRepository.findByUserIdOnDate(userId, new Date());
+
+        if (checkInOnSameDay) {
+            throw new Error()
+        }
+
         const checkIn = await this.checkInsRepository.create({
             gym_id: gymId,
             user_id: userId
