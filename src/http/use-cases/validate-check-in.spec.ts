604827ea6cf3da83ed8636e8ef9b1c2427a89809
NatanRei <nreischmura@gmail.com>
2023-06-12 14:33:42 -0300

    feat: late check in validation

--- src/http/use-cases/validate-check-in.spec.ts
+++ src/http/use-cases/validate-check-in.spec.ts
@@ -10,7 +10,7 @@ describe('Validate Check-in Use Case', () => {
     beforeEach(async () => {
         checkInsRepository = new InMemoryCheckInsRepository()
         sut = new ValidateCheckInUseCase(checkInsRepository)
-        // vi.useFakeTimers()
+        vi.useFakeTimers()
     })
 
     afterEach(() => {
@@ -35,6 +35,23 @@ describe('Validate Check-in Use Case', () => {
         await expect(() => sut.execute({
             checkInId: 'inexistent-check-in-id'
         })).rejects.toBeInstanceOf(ResourceNotFoundError)
+    })
+
+    it('should not be able to validate the check-in after 20 minutes of its creation', async () => {
+        vi.setSystemTime(new Date(2023, 0, 1, 13, 40))
+
+        const createdCheckIn = await checkInsRepository.create({
+            gym_id: 'gym-01',
+            user_id: 'user-01',
+        })
+
+        const twentyOneMinutesInMs = 1000 * 60 * 21
+
+        vi.advanceTimersByTime(twentyOneMinutesInMs)
+
+        await expect(() => sut.execute({
+            checkInId: createdCheckIn.id
+        })).rejects.toBeInstanceOf(Error)
     })
 
 })
--- src/http/use-cases/validate-check-in.ts
+++ src/http/use-cases/validate-check-in.ts
@@ -1,6 +1,8 @@
 import { CheckIn } from "@prisma/client";
 import { CheckInsRepository } from "../repositories/check-ins-repository";
 import { ResourceNotFoundError } from './errors/resource-not-found-error';
+import dayjs from "dayjs";
+import { LateCheckInValidationError } from "./errors/late-check-in-validation-error";
 
 interface ValidateCheckInUseCaseRequest {
     checkInId: string
@@ -19,6 +21,15 @@ export class ValidateCheckInUseCase {
 
         if (!checkIn) {
             throw new ResourceNotFoundError()
+        }
+
+        const distanceInMinutesFromCheckInCreation = dayjs(new Date()).diff(
+            checkIn.created_at,
+            'minutes'
+        )
+
+        if(distanceInMinutesFromCheckInCreation > 20) {
+            throw new LateCheckInValidationError()
         }
 
         checkIn.validated_at = new Date()
